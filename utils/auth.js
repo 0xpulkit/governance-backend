133479d81421118cc3d2e6780d7402ba552ab491
Raneet Debnath <raneetdebnath10@gmail.com>
2022-01-06 16:21:56 +0530

    fix auth

--- utils/auth.js
+++ utils/auth.js
@@ -2,16 +2,22 @@ require('dotenv').config();
 const Admin = require('../models/admin');
 
 async function authorize(req, res, next) {
-    const admin = Admin.findOne({
-        address: req.body.address}, 
-        {_id: 0, __v: 0}).lean();
-    
-    if (!admin) {
-        res.send(401).json({ message: "Unauthorized access" });
+    try {
+        const admin = await Admin.findOne({
+            address: req.body.address}, 
+            {_id: 0, __v: 0}).lean();
+        
+        if (!admin) {
+            res.sendStatus(401);
+        } else {
+            next();
+        }
+
+    } catch (error) {
+        console.log("error: ", error);
+        res.sendStatus(401);
     }
 
-    // TODO(raneet10): Verify admin from a set of addresses
-    next();
 }
 
 module.exports = authorize;
